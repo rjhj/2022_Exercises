2b6c802f8929085537bff33cb2b9614e68fae02d
unknown <rjhjuu@gmail.com>
2022-06-27 19:29:47 +0300

    Add tests

--- valid_parentheses.py
+++ valid_parentheses.py
@@ -19,7 +19,17 @@
 
 class Solution:
     def isValid(self, s: str) -> bool:
-        pass
+        return True
         
 
+solver = Solution()
 
+# Test data
+testStrings = ["()", "()[]{}", "(]", "{{{{", ")", "((([([])]))"]
+expectedResults = [True, True, False, False, False, True]
+
+i = 0
+while (i < len(testStrings)):
+    result = solver.isValid(testStrings[i])
+    print("Expected result: " + str(expectedResults[i]) + " Result: " + str(result) + " Was the result correct? " + str(expectedResults[i] == result))
+    i += 1
