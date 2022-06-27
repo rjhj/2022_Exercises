f272a478ed7911b748ce582148f80496d4bf6da1
unknown <rjhjuu@gmail.com>
2022-06-27 14:17:26 +0300

    Solved

--- palindrome_number.py
+++ palindrome_number.py
@@ -1,32 +1,33 @@
-"""
-Given an integer x, return true if x is palindrome integer.
 
-An integer is a palindrome when it reads the same backward as forward.
+# Given an integer x, return true if x is palindrome integer.
 
-For example, 121 is a palindrome while 123 is not.
+# An integer is a palindrome when it reads the same backward as forward.
+
+# For example, 121 is a palindrome while 123 is not.
  
 
-Example 1:
+# Example 1:
 
-Input: x = 121
-Output: true
-Explanation: 121 reads as 121 from left to right and from right to left.
-Example 2:
+# Input: x = 121
+# Output: true
+# Explanation: 121 reads as 121 from left to right and from right to left.
+# Example 2:
 
-Input: x = -121
-Output: false
-Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
-Example 3:
+# Input: x = -121
+# Output: false
+# Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+# Example 3:
 
-Input: x = 10
-Output: false
-Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
-"""
+# Input: x = 10
+# Output: false
+# Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
 
+
+#Solving is easy if an int is converted to a string.
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        return True
-
+        y = str(x)
+        return y == y[::-1]
 
 solver = Solution()
 
@@ -44,3 +45,7 @@ while i < len(inputs):
         print("Incorrect!")
     i += 1
 
+# Success
+# Details 
+# Runtime: 128 ms, faster than 16.50% of Python3 online submissions for Palindrome Number.
+# Memory Usage: 13.8 MB, less than 58.71% of Python3 online submissions for Palindrome Number.
