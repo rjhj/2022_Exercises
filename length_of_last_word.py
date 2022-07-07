255f5dd3e37802aefef38f4a1c4cd3b29770f508
unknown <rjhjuu@gmail.com>
2022-07-07 20:27:44 +0300

    Add length_of_last_word.py

---
+++ length_of_last_word.py
@@ -0,0 +1,53 @@
+# Given a string s consisting of words and spaces,
+# return the length of the last word in the string.
+
+# A word is a maximal substring consisting
+#  of non-space characters only.
+
+# Example 1:
+# Input: s = "Hello World"
+# Output: 5
+# Explanation: The last word is "World" with length 5.
+
+# Example 2:
+# Input: s = "   fly me   to   the moon  "
+# Output: 4
+# Explanation: The last word is "moon" with length 4.
+
+# Example 3:
+# Input: s = "luffy is still joyboy"
+# Output: 6
+# Explanation: The last word is "joyboy" with length 6.
+ 
+# Constraints:
+# 1 <= s.length <= 104
+# s consists of only English letters and spaces ' '.
+# There will be at least one word in s.
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        last = len(s)
+        while True:
+            if s[last-1].isspace():
+                last -= 1
+            else:
+                break
+        print(s[0:last+1])
+        first = last 
+        while first > 0:
+            if not s[first-1].isspace():
+                first -= 1
+            else:
+                return last - first
+        return last
+
+solution = Solution()
+
+#Test data
+testList = ["a", "aa", "Hello World", "   fly me   to   the moon  ", "luffy is still joyboy"]
+expectedResults = [1, 2, 5, 4, 6]
+
+#Tests
+for i in range(len(testList)):
+    result = solution.lengthOfLastWord(testList[i])
+    print(f"Expected: {expectedResults[i]} Result: {result}")
